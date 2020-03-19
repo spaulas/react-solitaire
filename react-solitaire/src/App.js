218f4d2b0df6f8c35e2a82f6630a9be18d956eca
spaulas <psantos@unknownb469212c2c07.lan>
2020-03-19 13:11:04 +0000

    Change Title

--- react-solitaire/src/App.js
+++ react-solitaire/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Solitaire Game
         </a>
       </header>
     </div>
