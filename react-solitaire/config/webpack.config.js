ff79c265b2fb71338257286bf42ec25df6abc3cf
spaulas <psantos@unknownb469212c2c07.lan>
2020-03-20 21:32:18 +0000

    added less

--- react-solitaire/config/webpack.config.js
+++ react-solitaire/config/webpack.config.js
@@ -440,6 +440,14 @@ module.exports = function(webpackEnv) {
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true
             },
+            {
+              test: /\.less$/,
+              use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                { loader: "less-loader", options: { javascriptEnabled: true } }
+              ]
+            },
             // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
             // using the extension .module.css
             {
