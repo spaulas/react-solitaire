026ae96e197e572526c890b18963d8e7b3c5a01f
spaulas <psantos@unknownb469212c2c07.lan>
2020-03-20 20:17:30 +0000

    Just basic components

---
+++ react-solitaire/src/react/Cards/CardSpot.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CardSpot() {
+  return <div> THIS IS A CARD</div>;
+}
+
+export default CardSpot;
---
+++ react-solitaire/src/react/Pages/GameBoard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameBoard() {
+  return <div className="gameBoard">this is the game board</div>;
+}
+
+export default GameBoard;
