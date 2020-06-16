e5a40651722f2ab780a0e401e1f7113895c20a97
PSantosWave <s.paula@ua.pt>
2020-06-16 17:10:21 +0100

    create conditions for actions choice

--- react-solitaire/src/react/components/Buttons/UndoButton.component.tsx
+++ react-solitaire/src/react/components/Buttons/UndoButton.component.tsx
@@ -1,14 +1,56 @@
+/* eslint-disable no-console */
+import { useDispatch, useSelector } from "react-redux";
 import React from "react";
+import { RootReducerState } from "../../../global";
 import { StepBackwardOutlined } from "@ant-design/icons";
-// import gameBoardActions from "../../../redux/gameBoard/gameBoard.actions";
-// import { useDispatch } from "react-redux";
+import gameBoardActions from "../../../redux/gameBoard/gameBoard.actions";
 
 /**
  * Option to undo a game move
  */
 function UndoButton() {
-  // const dispatch = useDispatch();
-  // eslint-disable-next-line no-console
-  return <StepBackwardOutlined onClick={() => console.log("UNDO ACTION")} />;
+  const dispatch = useDispatch();
+
+  // get gameMoves from redux
+  const { gamePreviousMoves } = useSelector(
+    ({ GameBoard }: RootReducerState) => ({
+      gamePreviousMoves: GameBoard.gamePreviousMoves
+    })
+  );
+
+  const handleUndo = () => {
+    const nMoves = gamePreviousMoves.length;
+    console.log("gamePreviousMoves = ", gamePreviousMoves);
+    // can only undo when there are moves to go back
+    if (nMoves > 0) {
+      const { source, target, nCards } = gamePreviousMoves[nMoves - 1];
+
+      if (source.indexOf("deckPile") === 0) {
+        if (target.indexOf("flippedPile") === 0) {
+          console.log("GO BACK from DECK pile to FLIPPED pile");
+        } else if (target.indexOf("goal") === 0) {
+          console.log("GO BACK from DECK pile to GOAL pile");
+        } else if (target.indexOf("column") === 0) {
+          console.log("GO BACK from DECK pile to COLUMN pile");
+        }
+      } else if (source.indexOf("column") === 0) {
+        if (target.indexOf("column") === 0) {
+          console.log("GO BACK from COLUMN pile to COLUMN pile");
+        } else if (target.indexOf("goal") === 0) {
+          console.log("GO BACK from COLUMN pile to GOAL pile");
+        }
+      } else if (source.indexOf("goal") === 0) {
+        if (target.indexOf("column") === 0) {
+          console.log("GO BACK from GOAL pile to COLUMN pile");
+        } else if (target.indexOf("goal") === 0) {
+          console.log("GO BACK from GOAL pile to GOAL pile");
+        }
+      } else {
+        console.log("GO BACK from FLIPPED pile to DECK pile");
+      }
+    }
+  };
+
+  return <StepBackwardOutlined onClick={handleUndo} />;
 }
 export default UndoButton;
