16c97c25039c4abcb2fa6ec7b2811913fec7b9c7
PSantosWave <s.paula@ua.pt>
2020-06-16 08:17:12 +0100

    add game pause option to timer

--- react-solitaire/src/react/components/Buttons/PauseGameButton.component.tsx
+++ react-solitaire/src/react/components/Buttons/PauseGameButton.component.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "antd";
 import PausedGameModal from "../Modals/PausedGameModal.component";
+import React from "react";
+import { RootReducerState } from "../../../global";
+import gameBoardActions from "../../../redux/gameBoard/gameBoard.actions";
 
 function PauseGameButton() {
-  const [visible, setVisible] = useState(false);
+  const dispatch = useDispatch();
+
+  // get the state for the game pause in game board redux
+  const { gamePaused } = useSelector(({ GameBoard }: RootReducerState) => ({
+    gamePaused: GameBoard.gamePaused
+  }));
+
+  // get the timeGame fuction
+  const timeGame = () => dispatch(gameBoardActions.timeGame());
+
   return (
     <>
-      <Button className="optionsButton" onClick={() => setVisible(true)}>
+      <Button className="optionsButton" onClick={timeGame}>
         Pause
       </Button>
-      <PausedGameModal visible={visible} closeModal={() => setVisible(false)} />
+      <PausedGameModal visible={gamePaused} closeModal={timeGame} />
     </>
   );
 }
