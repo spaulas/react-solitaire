be1bc90708a6b48a00534416469b5eb3760d1ba6
PSantosWave <s.paula@ua.pt>
2020-07-08 21:46:25 +0100

    add graphs to statistics page

---
+++ react-solitaire/src/react/pages/StatisticsPage/PieGraph.component.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+/* eslint-disable react/forbid-dom-props */
+/* eslint-disable react/no-multi-comp */
+import { Cell, Pie, PieChart, Tooltip } from "recharts";
+import React from "react";
+
+const data = [
+  { name: "Wins", value: 70 },
+  { name: "Losts", value: 30 }
+];
+
+const COLORS = ["rgba(0,0,0,0.6)", "rgba(0,0,0,0.4)"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index
+}: any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+function Example() {
+  return (
+    <PieChart width={500} height={600} className="statisticsPieChart">
+      <Pie
+        data={data}
+        cx={250}
+        cy={250}
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={150}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip
+        contentStyle={{
+          border: "none",
+          background: "white",
+          fontSize: "16px",
+          fontWeight: "bold",
+          borderRadius: "20px"
+        }}
+      />
+    </PieChart>
+  );
+}
+
+export default Example;
--- react-solitaire/src/react/pages/StatisticsPage/StatisticsPage.component.tsx
+++ react-solitaire/src/react/pages/StatisticsPage/StatisticsPage.component.tsx
@@ -1,7 +1,34 @@
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import BarGraph from "./BarGraph.component";
+import { Carousel } from "antd";
+import PageTitle from "../../components/UIComponents/PageTitle.component";
+import Piegraph from "./PieGraph.component";
 import React from "react";
 
-function ScoresPage() {
-  return <div className="pageBackground">This is the statistics page</div>;
+function StatisticsPage() {
+  return (
+    <div className="pageBackground statisticsPage">
+      <PageTitle title="Statistics" />
+      <div className="statisticsCarouselContainer">
+        <LeftOutlined className="carouselArrow carouselArrowLeft" />
+        <Carousel className="statisticsCarousel">
+          <div>
+            <div className="tabTitle">Win/Lost Ratio</div>
+            <Piegraph />
+          </div>
+          <div>
+            <span>Win/Lost</span>
+            <BarGraph />
+          </div>
+          <div>
+            <span>Win/Lost</span>
+            <BarGraph />
+          </div>
+        </Carousel>
+        <RightOutlined className="carouselArrow carouselArrowRight" />
+      </div>
+    </div>
+  );
 }
 
-export default ScoresPage;
+export default StatisticsPage;
