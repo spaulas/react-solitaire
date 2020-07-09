3d58a7c81436683e7219183673c206d87484c808
PSantosWave <s.paula@ua.pt>
2020-07-09 09:32:28 +0100

    organize breadcrumb

--- react-solitaire/src/react/components/PageTitle/PageTitle.component.tsx
+++ react-solitaire/src/react/components/PageTitle/PageTitle.component.tsx
@@ -1,4 +1,4 @@
-import BreadCrumb from "../../App/BreadCrumb.component";
+import BreadCrumb from "../BreadCrumb/BreadCrumb.component";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
