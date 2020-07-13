249e15820ae162ce81b6323033870fffab3676c1
PSantosWave <s.paula@ua.pt>
2020-07-13 14:05:27 +0100

    page start

--- react-solitaire/src/react/pages/ConfigurationsPage/ConfigurationsPage.component.tsx
+++ react-solitaire/src/react/pages/ConfigurationsPage/ConfigurationsPage.component.tsx
@@ -1,6 +1,12 @@
+import { FormattedMessage } from "react-intl";
+import PageTitle from "../../components/PageTitle/PageTitle.component";
 import React from "react";
 
 function ConfigurationsPage() {
-  return <div>titkle</div>;
+  return (
+    <div className="joyrideStartingPage startingPage">
+      <PageTitle title={<FormattedMessage id="sidebar.configurations" />} />
+    </div>
+  );
 }
 export default ConfigurationsPage;
