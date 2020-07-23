44d51da974b7c440f30601e92ae9a252dda4c800
PSantosWave <s.paula@ua.pt>
2020-07-23 19:28:17 +0100

    Updates

---
+++ precache-manifest.67633a9074888aab6529312827c59a99.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8af4df198c9de77415d37d3fa3c66ca",
+    "url": "/solitaireGame/index.html"
+  },
+  {
+    "revision": "40c6dcf94cdea6560a2c",
+    "url": "/solitaireGame/static/js/2.90ad7d10.chunk.js"
+  },
+  {
+    "revision": "4d2e1733b46b88cd30ea117cda7ae302",
+    "url": "/solitaireGame/static/js/2.90ad7d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e3bb62f748884b17e3",
+    "url": "/solitaireGame/static/js/main.e552338c.chunk.js"
+  },
+  {
+    "revision": "9f6e1a330fd621782398",
+    "url": "/solitaireGame/static/js/runtime-main.7f77371d.js"
+  },
+  {
+    "revision": "74d648b47fb46a5d92b1db8ae8574d84",
+    "url": "/solitaireGame/static/media/christmasTree.74d648b4.png"
+  },
+  {
+    "revision": "e6e29fd9661ce505b60f1bac0dc662be",
+    "url": "/solitaireGame/static/media/clubs01.e6e29fd9.png"
+  },
+  {
+    "revision": "4a30fc17a5afc359e9c0e32bff0caa8b",
+    "url": "/solitaireGame/static/media/clubs02.4a30fc17.png"
+  },
+  {
+    "revision": "31684514ba6b78922ae106d9b47a6c5e",
+    "url": "/solitaireGame/static/media/clubs03.31684514.png"
+  },
+  {
+    "revision": "badc3849a3c3c6e7cf618119c631702c",
+    "url": "/solitaireGame/static/media/clubs04.badc3849.png"
+  },
+  {
+    "revision": "92b78a4af37f0dfb50246948abc2990d",
+    "url": "/solitaireGame/static/media/clubs05.92b78a4a.png"
+  },
+  {
+    "revision": "5d6dc45775941025fc146827a840ce70",
+    "url": "/solitaireGame/static/media/clubs06.5d6dc457.png"
+  },
+  {
+    "revision": "cf687e7fd402fd16762b86be1c9b1829",
+    "url": "/solitaireGame/static/media/clubs07.cf687e7f.png"
+  },
+  {
+    "revision": "b772185aa4d50b38b28805040a6477c6",
+    "url": "/solitaireGame/static/media/clubs08.b772185a.png"
+  },
+  {
+    "revision": "638c2830cd094b8e2fbd72c4b6740559",
+    "url": "/solitaireGame/static/media/clubs09.638c2830.png"
+  },
+  {
+    "revision": "59bd77fd63bccb55a7a9caa9b7ebd3b4",
+    "url": "/solitaireGame/static/media/clubs10.59bd77fd.png"
+  },
+  {
+    "revision": "e5bcd6fb3bde1ea8012ea404562e30dd",
+    "url": "/solitaireGame/static/media/clubs11.e5bcd6fb.png"
+  },
+  {
+    "revision": "b4faff38b3bc5390568e46e79f2cdfcd",
+    "url": "/solitaireGame/static/media/clubs12.b4faff38.png"
+  },
+  {
+    "revision": "301dea0321b8970c4d9ef72a969114d5",
+    "url": "/solitaireGame/static/media/clubs13.301dea03.png"
+  },
+  {
+    "revision": "c52bfdcd23eec93d6ec9dc535b22a342",
+    "url": "/solitaireGame/static/media/developer.c52bfdcd.jpg"
+  },
+  {
+    "revision": "023ebfc2eba4d1a07b1721c016cc84ec",
+    "url": "/solitaireGame/static/media/diamonds01.023ebfc2.png"
+  },
+  {
+    "revision": "7c971de81994f3a25a153e0288aea459",
+    "url": "/solitaireGame/static/media/diamonds02.7c971de8.png"
+  },
+  {
+    "revision": "453085b502912f81c79723165ff5ed91",
+    "url": "/solitaireGame/static/media/diamonds03.453085b5.png"
+  },
+  {
+    "revision": "21b584a08038001e671d5ddf135b82e1",
+    "url": "/solitaireGame/static/media/diamonds04.21b584a0.png"
+  },
+  {
+    "revision": "3b8f511ac19b1e10ced8e6ecf6bc59c1",
+    "url": "/solitaireGame/static/media/diamonds05.3b8f511a.png"
+  },
+  {
+    "revision": "8071ba22ab6f96b5e2df6807e7c8b95f",
+    "url": "/solitaireGame/static/media/diamonds06.8071ba22.png"
+  },
+  {
+    "revision": "f907d97054559f4774ecda8e148a24b8",
+    "url": "/solitaireGame/static/media/diamonds07.f907d970.png"
+  },
+  {
+    "revision": "57c8dfb7abf4f95546f0778400625d75",
+    "url": "/solitaireGame/static/media/diamonds08.57c8dfb7.png"
+  },
+  {
+    "revision": "f972c9ec6b4681a422acd222cc2c40a4",
+    "url": "/solitaireGame/static/media/diamonds09.f972c9ec.png"
+  },
+  {
+    "revision": "8f86020dd1a69b6987f562eff9f89f4e",
+    "url": "/solitaireGame/static/media/diamonds10.8f86020d.png"
+  },
+  {
+    "revision": "dfd23aa05782bebba56c429ad19d306b",
+    "url": "/solitaireGame/static/media/diamonds11.dfd23aa0.png"
+  },
+  {
+    "revision": "86986744591258ff4831310d853a9ec8",
+    "url": "/solitaireGame/static/media/diamonds12.86986744.png"
+  },
+  {
+    "revision": "e862c60342cdb4b55f35cdcec1c0cb6b",
+    "url": "/solitaireGame/static/media/diamonds13.e862c603.png"
+  },
+  {
+    "revision": "f24e3a514645b385f892dadaf51b05d4",
+    "url": "/solitaireGame/static/media/flowers.f24e3a51.png"
+  },
+  {
+    "revision": "ed9b17666f8ee99db2f8c03142736f9a",
+    "url": "/solitaireGame/static/media/hearts01.ed9b1766.png"
+  },
+  {
+    "revision": "47f84fbf432094f30698b464306c5803",
+    "url": "/solitaireGame/static/media/hearts02.47f84fbf.png"
+  },
+  {
+    "revision": "a93d142354fb29a555605e67b9e4b135",
+    "url": "/solitaireGame/static/media/hearts03.a93d1423.png"
+  },
+  {
+    "revision": "d20bce9b3522cc05e791b717a82d940f",
+    "url": "/solitaireGame/static/media/hearts04.d20bce9b.png"
+  },
+  {
+    "revision": "2f72dc7c6cfeeec7cbba62e57ccfed0d",
+    "url": "/solitaireGame/static/media/hearts05.2f72dc7c.png"
+  },
+  {
+    "revision": "2dd40e5b9aaeee17da83595abd3ec900",
+    "url": "/solitaireGame/static/media/hearts06.2dd40e5b.png"
+  },
+  {
+    "revision": "7b0880a8d2f1b32db0dffdd6e5605083",
+    "url": "/solitaireGame/static/media/hearts07.7b0880a8.png"
+  },
+  {
+    "revision": "c10ff2f331b099c5c7d76cb829d020d4",
+    "url": "/solitaireGame/static/media/hearts08.c10ff2f3.png"
+  },
+  {
+    "revision": "77106d03076540ffdf345b5eeef4c5e2",
+    "url": "/solitaireGame/static/media/hearts09.77106d03.png"
+  },
+  {
+    "revision": "9b181b55b9a08b532e9bc9c8a24dc673",
+    "url": "/solitaireGame/static/media/hearts10.9b181b55.png"
+  },
+  {
+    "revision": "30573dba2332ef9880adc8912350d78d",
+    "url": "/solitaireGame/static/media/hearts11.30573dba.png"
+  },
+  {
+    "revision": "5f01204fa48b9003c77ab8e1af489f09",
+    "url": "/solitaireGame/static/media/hearts12.5f01204f.png"
+  },
+  {
+    "revision": "5ccf3673a4b836337858fb1674ce8301",
+    "url": "/solitaireGame/static/media/hearts13.5ccf3673.png"
+  },
+  {
+    "revision": "81851c8ed184f3e3e58f8f5a12c71f37",
+    "url": "/solitaireGame/static/media/honeyComb.81851c8e.png"
+  },
+  {
+    "revision": "5fea2d1c51731cad54c2c9a0088e28db",
+    "url": "/solitaireGame/static/media/icon.5fea2d1c.png"
+  },
+  {
+    "revision": "db870b3861bf60760cebb7f0bfe1cf27",
+    "url": "/solitaireGame/static/media/spades01.db870b38.png"
+  },
+  {
+    "revision": "98088f8f5d2624902ea4eed14644c8e9",
+    "url": "/solitaireGame/static/media/spades02.98088f8f.png"
+  },
+  {
+    "revision": "eb2f9397a7e14c0e7096c513e8d8c143",
+    "url": "/solitaireGame/static/media/spades03.eb2f9397.png"
+  },
+  {
+    "revision": "3c252c86e9d9823964bbb1ce849169ee",
+    "url": "/solitaireGame/static/media/spades04.3c252c86.png"
+  },
+  {
+    "revision": "909cf81e9edb9873b25e83d231f9d0e1",
+    "url": "/solitaireGame/static/media/spades05.909cf81e.png"
+  },
+  {
+    "revision": "06fc90c250473786f97f716f17a19820",
+    "url": "/solitaireGame/static/media/spades06.06fc90c2.png"
+  },
+  {
+    "revision": "8084cefce29bd244c9a82b49f6f30b56",
+    "url": "/solitaireGame/static/media/spades07.8084cefc.png"
+  },
+  {
+    "revision": "a217a46b435ed7ff801d308e510fb3e3",
+    "url": "/solitaireGame/static/media/spades08.a217a46b.png"
+  },
+  {
+    "revision": "a7fc5a9ee863d96a86e3b3bbbbfd7503",
+    "url": "/solitaireGame/static/media/spades09.a7fc5a9e.png"
+  },
+  {
+    "revision": "9d7574b752dddf8c2b57b3f050cea0af",
+    "url": "/solitaireGame/static/media/spades10.9d7574b7.png"
+  },
+  {
+    "revision": "e901813c345b5627c98da4a8fa5d7461",
+    "url": "/solitaireGame/static/media/spades11.e901813c.png"
+  },
+  {
+    "revision": "9bcc37352a4cbf61e23fdab056f3f633",
+    "url": "/solitaireGame/static/media/spades12.9bcc3735.png"
+  },
+  {
+    "revision": "e2ad5f815b5ea68dbca19291a452d63b",
+    "url": "/solitaireGame/static/media/spades13.e2ad5f81.png"
+  }
+]);
