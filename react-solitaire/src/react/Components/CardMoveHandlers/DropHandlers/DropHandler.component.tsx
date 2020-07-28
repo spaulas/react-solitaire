3ee7b98225d063899645b40ab29a64943118b4fa
PSantosWave <s.paula@ua.pt>
2020-07-28 16:31:46 +0100

    correct on drop function

--- react-solitaire/src/react/Components/CardMoveHandlers/DropHandlers/DropHandler.component.tsx
+++ react-solitaire/src/react/Components/CardMoveHandlers/DropHandlers/DropHandler.component.tsx
@@ -58,8 +58,21 @@ const DropHandler = ({
       document.getElementById("baseEmptySpots")?.offsetWidth || 1;
     const innerHeight = window.innerHeight;
     // get column size
-    const initialOffset = (innerWidth / 24) * 3;
+    const initialOffset = ((innerWidth / 24) * 3) / 8;
     const columnSizes = (innerWidth - initialOffset) / 7;
+
+    // eslint-disable-next-line no-console
+    console.log("INNER WIDTH = ", innerWidth);
+    // eslint-disable-next-line no-console
+    console.log("innerHeight = ", innerHeight);
+    // eslint-disable-next-line no-console
+    console.log("initialOffset = ", initialOffset);
+    // eslint-disable-next-line no-console
+    console.log("columnSizes = ", columnSizes);
+    // eslint-disable-next-line no-console
+    console.log("x = ", x);
+    // eslint-disable-next-line no-console
+    console.log("y = ", y);
 
     // should drop in one of the goal spots
     if (y < innerHeight / 3.8) {
