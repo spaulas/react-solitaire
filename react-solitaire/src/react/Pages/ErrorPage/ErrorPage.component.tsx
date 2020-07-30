591f79775afb08fd161ea761457df5ca212098aa
PSantosWave <s.paula@ua.pt>
2020-07-30 14:18:24 +0100

    create error page

--- react-solitaire/src/react/Pages/ErrorPage/ErrorPage.component.tsx
+++ react-solitaire/src/react/Pages/ErrorPage/ErrorPage.component.tsx
@@ -1,7 +1,24 @@
+import { FormattedMessage } from "react-intl";
+import MenuButton from "../../Components/Buttons/MenuButton.component";
 import React from "react";
+import { WarningFilled } from "@ant-design/icons";
 
-// To be done  in the future
 function ErrorPage() {
-  return <div className=" mainPage">An error as occured</div>;
+  return (
+    <div className="mainPage errorPage">
+      <WarningFilled className="warningIcon" />
+      <span className="title">
+        <FormattedMessage id="error.title" />
+      </span>
+      <span className="subtitle">
+        <FormattedMessage id="error.subtitle" />
+      </span>
+      <MenuButton location="/">
+        <div className="menuButton">
+          <FormattedMessage id="error.goHome" />
+        </div>
+      </MenuButton>
+    </div>
+  );
 }
 export default ErrorPage;
